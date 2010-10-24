089ebf48341b0b9815e95be0cc477b45aa36e77c
Daniel Kirstenpfad <daniel@sones.de>
2010-10-24 15:05:06 +0200

    * updated Revision in Version.cs - removed unnecessary binary stuff

--- FFN-GUI-Switcher/Version.cs
+++ FFN-GUI-Switcher/Version.cs
@@ -7,8 +7,8 @@ namespace FFN_Switcher
 {
     public static class Version
     {
-        public static Int32 VersionNumber = 109;
-        public static String VersionString = "1.0.0.109";
+        public static Int32 VersionNumber = 110;
+        public static String VersionString = "1.0.0.110";
         public static String CopyrightString = "(C) 2008-2010 Daniel Kirstenpfad - http://www.schrankmonster.de";
         public static String LicenseString = "Licensed under Attribution-NonCommercial-ShareAlike 3.0 Unported license";
         public static String UpdateURL = "http://dropbox.schrankmonster.de/dropped/FFNSwitcher.xml";
